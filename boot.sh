502f7bc2dfab1b96b15578dae10299bd3f685e48
Rafael García <bladecoder@gmail.com>
2025-09-11 00:10:05 +0200

    cleanup boot.sh

--- boot.sh
+++ boot.sh
@@ -10,13 +10,16 @@ ZIP_URL="$REPO_URL/archive/refs/heads/main.zip"
 TMP_DIR="/tmp/omakase-ubuntu-setup-main"
 
 clear
-echo -e "\033[35m$ansi_art\033[0m"
+echo -e "\033[35m$ansi_art\033[0m\n"
+
+rm -rf "$TMP_DIR"
+rm -f /tmp/omakase-ubuntu-setup.zip
 
 echo "Downloading project zip..."
-wget -O /tmp/omakase-ubuntu-setup.zip "$ZIP_URL"
+wget -q -O /tmp/omakase-ubuntu-setup.zip "$ZIP_URL"
 
 echo "Unzipping to $TMP_DIR..."
-unzip -o /tmp/omakase-ubuntu-setup.zip -d /tmp/
+unzip -q /tmp/omakase-ubuntu-setup.zip -d /tmp/
 
 echo "Running install.sh..."
 chmod +x "$TMP_DIR/install.sh"
